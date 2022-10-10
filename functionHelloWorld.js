79e6d82ed617b5b982735e32fa621760e03e47c1
Mariela <mvmihilova@gmail.com>
2022-10-09 23:55:50 -0600

    added function hello world

---
+++ functionHelloWorld.js
@@ -0,0 +1,12 @@
+function greet(String) {
+  return String;
+} 
+const answer = greet('Hello world!');
+console.log(answer);
+function greet() {
+  return "hello world!";
+  }
+//in codewars the code that works is:
+function greet() {
+  return "hello world!";
+  }
