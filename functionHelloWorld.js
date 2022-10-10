fcf4043b7966607474415dfdfc196b3ecf758c21
Mariela <mvmihilova@gmail.com>
2022-10-10 01:56:14 -0600

    simplified hello world function

--- functionHelloWorld.js
+++ functionHelloWorld.js
@@ -1,11 +1,11 @@
-function greet(String) {
-  return String;
+function greeting(){
+  console.log("Hello world");
 } 
-const answer = greet('Hello world!');
-console.log(answer);
-function greet() {
-  return "hello world!";
-  }
+
+greeting();
+
+
+  
 //in codewars the code that works is:
 function greet() {
   return "hello world!";
