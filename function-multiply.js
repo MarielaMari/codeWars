03bed1e6335d7646ed01fa76d742c6631bb4634b
Mariela <mvmihilova@gmail.com>
2022-10-12 13:30:03 -0600

    added one more way

--- function-multiply.js
+++ function-multiply.js
@@ -2,3 +2,13 @@ function multiply(a, b){
     return a * b
   }
   console.log(multiply);
+
+  //another way is
+  
+  const a = 2
+const b = 3
+function multiply(a, b) {
+    return c = a * b;
+}
+multiply(2,3);
+console.log(`${c}`);
